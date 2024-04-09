df3b98949a091836104cffbb33aa224d8877de2b
benedixit <luciusemuh@gmail.com>
2024-04-09 21:43:39 +0100

    updated settings.py

--- quote/settings.py
+++ quote/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-tiaos4%09b2ysbzj-0gv%+-cdkgds5*_$47o&rezvo-1$i=z68
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['5fa9-102-67-1-64.ngrok-free.app', '127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['5fa9-102-67-1-64.ngrok-free.app', '127.0.0.1', '.vercel.app', '.now.sh']
 
 TAILWINDCSS_CLI_FILE = BASE_DIR / 'tailwindcss-windows-x64.exe'
 TAILWINDCSS_CONFIG_FILE = BASE_DIR / 'tailwind.config.js'
