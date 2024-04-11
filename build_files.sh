82d14ab5e8f61321c7b41c8ea5d79190d6496f04
benedixit <luciusemuh@gmail.com>
2024-04-11 12:38:12 +0100

    update build_files

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,8 @@
 echo "BUILD START" 
-<<<<<<< HEAD
 python -m ensure pip 
+pip install django
+python manage.py makemigrations
+python manage.py migrate
 python -m pip install -r requirements.txt 
 python manage.py collectstatic --noinput --clear 
 echo "BUILD END"
-=======
-python3.9 -m ensurepip 
-python3.9 -m pip install -r requirements.txt 
-python3.9 manage.py collectstatic
-echo "BUILD END"
->>>>>>> f68ab5b7fe624ab13f0de34138b4310974e0340a
